380798a290143fa47371ddd378e3bd458132fe8f
Alex Mit <klepoz.672@mail.ru>
2025-12-01 20:12:40 +0300

    initial backend/ui threading

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,59 @@
-use macroquad::color::Color;
+use std::{
+    sync::{Arc, RwLock},
+    time::Duration,
+};
+
+use macroquad::color::{Color, GREEN};
 
 #[derive(Clone, Copy)]
 pub struct Block {
-    color: Color,
+    pub color: Color,
 }
 
 const PLAYFIELD_ROWS: usize = 20;
 const PLAYFIELD_COLS: usize = 10;
 
-pub static playfield: [[Option<Block>; PLAYFIELD_COLS]; PLAYFIELD_ROWS] =
-    [[None; PLAYFIELD_COLS]; PLAYFIELD_ROWS];
+pub struct GameState {
+    pub playfield: Playfield,
+}
+
+impl GameState {
+    pub fn new() -> GameState {
+        GameState {
+            playfield: Playfield::new(None, None),
+        }
+    }
+    pub fn update(&mut self, row: usize, col: usize) {
+        self.playfield.field[row][col] = match self.playfield.field[row][col] {
+            Some(_) => None,
+            None => Some(Block { color: GREEN }),
+        }
+    }
+}
+
+pub struct Playfield {
+    pub field: Vec<Vec<Option<Block>>>,
+}
+
+impl Playfield {
+    fn new(rows: Option<usize>, cols: Option<usize>) -> Playfield {
+        Playfield {
+            field: vec![vec![None; rows.unwrap_or(PLAYFIELD_COLS)]; cols.unwrap_or(PLAYFIELD_ROWS)],
+        }
+    }
+}
+
+pub fn game_loop(game_state: Arc<RwLock<GameState>>) {
+    let mut cur_row = 0;
+    loop {
+        println!("Entered WACKY loop!");
+        {
+            let mut guard = game_state.write().unwrap();
+            guard.update(cur_row, 5);
+            cur_row += 1;
+            cur_row %= PLAYFIELD_ROWS;
+        }
+        println!("Processed logic!");
+        std::thread::sleep(Duration::from_millis(50));
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,50 @@
+use std::{
+    sync::{Arc, RwLock},
+    thread,
+};
+
 use macroquad::prelude::*;
-use tetrs::playfield;
-
-
+use tetrs::GameState;
 
 #[macroquad::main("MyGame")]
 async fn main() {
+    let game_state = Arc::new(RwLock::new(GameState::new()));
+
+    let state_clone = game_state.clone();
+
+    thread::spawn(move || {
+        tetrs::game_loop(state_clone);
+    });
+
     loop {
+        println!("Entered UI loop!");
         clear_background(BLACK);
 
         let mut x_shift = 0.;
         let mut y_shift = 0.;
-        for row in playfield {
-            for col in row {
-                draw_rectangle(x_shift, y_shift, 10.0, 10.0, GREEN);
-                x_shift += 15.;
+
+        {
+            let guard = game_state.read().unwrap();
+
+            for row in &guard.playfield.field {
+                for col in row {
+                    match col {
+                        Some(block) => {
+                            draw_rectangle(x_shift, y_shift, 10.0, 10.0, block.color);
+                        }
+                        None => {
+                            draw_rectangle(x_shift, y_shift, 10.0, 10.0, GRAY);
+                        }
+                    }
+
+                    x_shift += 15.;
+                }
+                y_shift += 15.;
+                x_shift = 0.;
             }
-            y_shift += 15.;
-            x_shift = 0.;
         }
 
-
+        println!("Drew a frame!");
         next_frame().await
     }
 }
