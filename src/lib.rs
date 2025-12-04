2bdd0db4446cb06108eb6ce01d0cbf8c38d1cc2c
Alex Mit <klepoz.672@mail.ru>
2025-12-04 03:24:01 +0300

    single threaded now

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,19 @@
 use std::{
+    collections::HashSet,
     sync::{Arc, RwLock},
-    time::Duration,
+    usize,
 };
 
-use macroquad::color::{Color, GREEN};
+use macroquad::{
+    color::{Color, GREEN},
+    input::KeyCode,
+    time::get_frame_time,
+};
+
+#[derive(Debug)]
+pub struct InputEvent {
+    pub keys: HashSet<KeyCode>,
+}
 
 #[derive(Clone, Copy)]
 pub struct Block {
@@ -15,24 +25,93 @@ const PLAYFIELD_COLS: usize = 10;
 
 pub struct GameState {
     pub playfield: Playfield,
+    pub descend_delay: Timer_ms,
+    pub column_toggling: isize,
+    pub row_toggleing: isize,
 }
 
 impl GameState {
     pub fn new() -> GameState {
         GameState {
             playfield: Playfield::new(None, None),
+            descend_delay: Timer_ms::new(10000),
+            column_toggling: 5,
+            row_toggleing: 0,
         }
     }
-    pub fn update(&mut self, row: usize, col: usize) {
+    pub fn toggle_block(&mut self, row: usize, col: usize) {
         self.playfield.field[row][col] = match self.playfield.field[row][col] {
             Some(_) => None,
             None => Some(Block { color: GREEN }),
         }
     }
+    pub fn toggle_block_in_row(&mut self) {
+        self.descend_delay.update();
+        if self.descend_delay.is_out() {
+            self.playfield.field[self.row_toggleing as usize][self.column_toggling as usize] =
+                match self.playfield.field[self.row_toggleing as usize]
+                    [self.column_toggling as usize]
+                {
+                    Some(_) => None,
+                    None => Some(Block { color: GREEN }),
+                };
+            self.row_toggleing = (self.row_toggleing + 1).rem_euclid(PLAYFIELD_ROWS as isize);
+            self.descend_delay.reset();
+        }
+    }
+    pub fn update_column_toggling_relative(&mut self, value: isize) {
+        self.column_toggling = match value.cmp(&(0 as isize)) {
+            std::cmp::Ordering::Less => {
+                (self.column_toggling - value.abs()).rem_euclid(PLAYFIELD_COLS as isize)
+            }
+            std::cmp::Ordering::Greater => {
+                (self.column_toggling + value).rem_euclid(PLAYFIELD_COLS as isize)
+            }
+            _ => self.column_toggling,
+        };
+    }
 }
 
+#[derive(Clone)]
 pub struct Playfield {
     pub field: Vec<Vec<Option<Block>>>,
+}
+
+#[derive(Clone, Copy)]
+pub struct Timer_ms {
+    time_start: std::time::Instant,
+    elapsed: i64,
+    wait: i64,
+}
+
+impl Timer_ms {
+    pub fn new(wait: i64) -> Self {
+        Self {
+            time_start: std::time::Instant::now(),
+            elapsed: 0,
+            wait: wait,
+        }
+    }
+    pub fn update(&mut self) -> bool {
+        self.elapsed += self.time_start.elapsed().as_millis() as i64;
+        self.is_out()
+    }
+    pub fn is_out(&self) -> bool {
+        self.wait <= (self.elapsed)
+    }
+    pub fn reset(&mut self) {
+        while self.is_out() {
+            self.elapsed -= self.wait;
+        }
+        self.time_start = std::time::Instant::now();
+        // self.elapsed = 0;
+    }
+    pub fn elapsed(&self) -> i64 {
+        self.elapsed
+    }
+    pub fn time_left(&self) -> i64 {
+        self.wait - self.elapsed
+    }
 }
 
 impl Playfield {
@@ -43,17 +122,12 @@ impl Playfield {
     }
 }
 
-pub fn game_loop(game_state: Arc<RwLock<GameState>>) {
-    let mut cur_row = 0;
-    loop {
-        println!("Entered WACKY loop!");
-        {
-            let mut guard = game_state.write().unwrap();
-            guard.update(cur_row, 5);
-            cur_row += 1;
-            cur_row %= PLAYFIELD_ROWS;
-        }
-        println!("Processed logic!");
-        std::thread::sleep(Duration::from_millis(50));
-    }
+pub fn process_logic(game_state: &mut GameState, input: InputEvent) {
+    // let mut descend_delay = Timer_ms::new(10);
+    // descend_delay.update(frame_time_s.elapsed().as_millis() as i64);
+    // if descend_delay.is_out() {
+    // descend_delay.reset();
+    // frame_time_s = std::time::Instant::now();
+    game_state.toggle_block_in_row();
+    // }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,55 @@
 use std::{
-    sync::{Arc, RwLock},
+    collections::HashSet,
+    sync::{mpsc, Arc, RwLock},
     thread,
+    time::Duration,
 };
 
 use macroquad::prelude::*;
-use tetrs::GameState;
+use tetrs::InputEvent;
+use tetrs::{process_logic, GameState};
 
 #[macroquad::main("MyGame")]
 async fn main() {
-    let game_state = Arc::new(RwLock::new(GameState::new()));
-
-    let state_clone = game_state.clone();
-
-    thread::spawn(move || {
-        tetrs::game_loop(state_clone);
-    });
+    let mut game_state = GameState::new();
+    let mut frame_counter = 0;
+    let mut input_buf = HashSet::<KeyCode>::new();
+    let mut time_start = std::time::Instant::now();
 
     loop {
+        let inputs = InputEvent {
+            keys: get_keys_pressed(),
+        };
+
+        process_logic(&mut game_state, inputs);
+
         println!("Entered UI loop!");
         clear_background(BLACK);
 
         let mut x_shift = 0.;
-        let mut y_shift = 0.;
+        let mut y_shift = 50.;
 
-        {
-            let guard = game_state.read().unwrap();
-
-            for row in &guard.playfield.field {
-                for col in row {
-                    match col {
-                        Some(block) => {
-                            draw_rectangle(x_shift, y_shift, 10.0, 10.0, block.color);
-                        }
-                        None => {
-                            draw_rectangle(x_shift, y_shift, 10.0, 10.0, GRAY);
-                        }
+        for row in &game_state.playfield.field {
+            for col in row {
+                match col {
+                    Some(block) => {
+                        draw_rectangle(x_shift, y_shift, 10.0, 10.0, block.color);
                     }
-
-                    x_shift += 15.;
+                    None => {
+                        draw_rectangle(x_shift, y_shift, 10.0, 10.0, GRAY);
+                    }
                 }
-                y_shift += 15.;
-                x_shift = 0.;
+
+                x_shift += 15.;
             }
+            y_shift += 15.;
+            x_shift = 0.;
         }
 
+        draw_fps();
+        frame_counter += 1;
+
         println!("Drew a frame!");
-        next_frame().await
+        next_frame().await;
     }
 }
