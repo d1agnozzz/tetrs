e8e5f816a1c0ed593c20af30d22fdd8e724d6abb
Alex Mit <klepoz.672@mail.ru>
2025-12-07 08:44:16 +0300

    some gameplay logic implemented

--- src/lib.rs
+++ src/lib.rs
@@ -1,90 +1,289 @@
 use std::{
     collections::HashSet,
-    sync::{Arc, RwLock},
-    usize,
+    hash::Hash,
+    ops::{Add, AddAssign, Bound, Mul, Rem, RemAssign, Sub, SubAssign},
+    process::Output,
 };
 
 use macroquad::{
-    color::{Color, GREEN},
+    color::{Color, BLUE, GREEN, RED},
     input::KeyCode,
-    time::get_frame_time,
 };
+use std::time::Duration;
 
 #[derive(Debug)]
 pub struct InputEvent {
     pub keys: HashSet<KeyCode>,
 }
 
+#[derive(PartialEq, Eq, Hash, Clone, Copy)]
+pub struct BlockCoordinates {
+    pub row: isize,
+    pub col: isize,
+}
+
+impl Add<[isize; 2]> for BlockCoordinates {
+    type Output = BlockCoordinates;
+
+    fn add(self, rhs: [isize; 2]) -> Self::Output {
+        BlockCoordinates {
+            row: self.row + rhs[0],
+            col: self.col + rhs[1],
+        }
+    }
+}
+
 #[derive(Clone, Copy)]
 pub struct Block {
     pub color: Color,
+    pub coordinates: BlockCoordinates,
 }
 
-const PLAYFIELD_ROWS: usize = 20;
-const PLAYFIELD_COLS: usize = 10;
+impl PartialEq for Block {
+    fn eq(&self, other: &Self) -> bool {
+        self.coordinates == other.coordinates
+    }
+}
+
+impl Eq for Block {}
+
+impl Hash for Block {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        self.coordinates.hash(state);
+    }
+}
+
+const PLAYFIELD_ROWS: isize = 20;
+const PLAYFIELD_COLS: isize = 10;
+
+pub struct TetraminoShape {
+    pub blocks: HashSet<Block>,
+}
+
+#[derive(Clone, Copy)]
+pub struct Bounds {
+    pub x_min: isize,
+    pub x_max: isize,
+    pub y_min: isize,
+    pub y_max: isize,
+}
+
+impl Bounds {
+    fn new() -> Bounds {
+        Bounds {
+            x_min: 0,
+            x_max: 0,
+            y_min: 0,
+            y_max: 0,
+        }
+    }
+    fn update(&self, (x, y): (isize, isize)) -> Bounds {
+        Bounds {
+            x_min: self.x_min.min(x),
+            x_max: self.x_max.max(x),
+            y_min: self.y_min.min(y),
+            y_max: self.y_max.max(y),
+        }
+    }
+}
+
+pub struct BoundingBox {
+    width: isize,
+    height: isize,
+}
+
+impl From<Bounds> for BoundingBox {
+    fn from(value: Bounds) -> Self {
+        BoundingBox {
+            width: value.x_max - value.x_min,
+            height: value.y_max - value.y_min,
+        }
+    }
+}
+
+impl TetraminoShape {
+    fn stick() -> TetraminoShape {
+        TetraminoShape {
+            blocks: {
+                vec![
+                    Block {
+                        coordinates: BlockCoordinates { row: 0, col: 0 },
+                        color: BLUE,
+                    },
+                    Block {
+                        coordinates: BlockCoordinates { row: 1, col: 0 },
+                        color: BLUE,
+                    },
+                    Block {
+                        coordinates: BlockCoordinates { row: 2, col: 0 },
+                        color: BLUE,
+                    },
+                    Block {
+                        coordinates: BlockCoordinates { row: 3, col: 0 },
+                        color: BLUE,
+                    },
+                ]
+                .into_iter()
+                .collect()
+            },
+        }
+    }
+    fn get_bounding_box(&self) -> BoundingBox {
+        self.blocks
+            .iter()
+            .fold(Bounds::new(), |acc: Bounds, b: &Block| {
+                acc.update((b.coordinates.col, b.coordinates.row))
+            })
+            .into()
+    }
+}
+
+#[derive(Default, Clone, Copy)]
+pub struct OffsetPosition {
+    pub x_offset: isize,
+    pub y_offset: isize,
+}
+
+impl AddAssign for OffsetPosition {
+    fn add_assign(&mut self, rhs: Self) {
+        self.x_offset += rhs.x_offset;
+        self.y_offset += rhs.y_offset;
+    }
+}
+
+impl RemAssign<PlayfieldSize> for OffsetPosition {
+    fn rem_assign(&mut self, rhs: PlayfieldSize) {
+        self.x_offset %= rhs.cols;
+        self.y_offset %= rhs.rows;
+    }
+}
+
+impl SubAssign<BoundingBox> for OffsetPosition {
+    fn sub_assign(&mut self, rhs: BoundingBox) {
+        self.x_offset -= rhs.height;
+        self.y_offset -= rhs.width;
+    }
+}
+
+pub struct FallingTetramino {
+    pub shape: TetraminoShape,
+    pub offset: OffsetPosition,
+}
+
+impl FallingTetramino {
+    fn new(shape: TetraminoShape) -> FallingTetramino {
+        FallingTetramino {
+            shape: shape,
+            offset: OffsetPosition::default(),
+        }
+    }
+}
+
+#[derive(Clone, Copy, PartialEq, PartialOrd)]
+pub struct PlayfieldSize {
+    pub rows: isize,
+    pub cols: isize,
+}
+
+pub struct PlacedBlocks {
+    storage: HashSet<Block>,
+}
 
 pub struct GameState {
-    pub playfield: Playfield,
-    pub descend_delay: Timer_ms,
+    pub playfield_size: PlayfieldSize,
+    pub placed_blocks: HashSet<Block>,
+    pub current_tetramino: FallingTetramino,
+    pub next_tetramino: TetraminoShape,
+    pub descend_delay: TimerMs,
     pub column_toggling: isize,
     pub row_toggleing: isize,
 }
 
+impl Sub<BoundingBox> for PlayfieldSize {
+    type Output = PlayfieldSize;
+
+    fn sub(self, rhs: BoundingBox) -> Self::Output {
+        PlayfieldSize {
+            rows: self.rows - rhs.height,
+            cols: self.cols - rhs.width,
+        }
+    }
+}
+
 impl GameState {
+    pub fn get_playfield_center(&self) -> OffsetPosition {
+        OffsetPosition {
+            x_offset: self.playfield_size.cols / 2,
+            y_offset: self.playfield_size.rows / 2,
+        }
+    }
     pub fn new() -> GameState {
         GameState {
-            playfield: Playfield::new(None, None),
-            descend_delay: Timer_ms::new(10000),
+            playfield_size: PlayfieldSize { rows: 20, cols: 10 },
+            placed_blocks: Default::default(),
+            descend_delay: TimerMs::new(1000),
+            current_tetramino: FallingTetramino::new(TetraminoShape::stick()),
+            next_tetramino: TetraminoShape::stick(),
             column_toggling: 5,
             row_toggleing: 0,
         }
     }
-    pub fn toggle_block(&mut self, row: usize, col: usize) {
-        self.playfield.field[row][col] = match self.playfield.field[row][col] {
-            Some(_) => None,
-            None => Some(Block { color: GREEN }),
+    pub fn reset_tetramino_offset(&mut self) {
+        self.current_tetramino.offset = OffsetPosition {
+            x_offset: self.playfield_size.cols / 2,
+            y_offset: self.playfield_size.rows / 2,
         }
     }
-    pub fn toggle_block_in_row(&mut self) {
-        self.descend_delay.update();
-        if self.descend_delay.is_out() {
-            self.playfield.field[self.row_toggleing as usize][self.column_toggling as usize] =
-                match self.playfield.field[self.row_toggleing as usize]
-                    [self.column_toggling as usize]
-                {
-                    Some(_) => None,
-                    None => Some(Block { color: GREEN }),
-                };
-            self.row_toggleing = (self.row_toggleing + 1).rem_euclid(PLAYFIELD_ROWS as isize);
-            self.descend_delay.reset();
-        }
-    }
-    pub fn update_column_toggling_relative(&mut self, value: isize) {
-        self.column_toggling = match value.cmp(&(0 as isize)) {
-            std::cmp::Ordering::Less => {
-                (self.column_toggling - value.abs()).rem_euclid(PLAYFIELD_COLS as isize)
-            }
-            std::cmp::Ordering::Greater => {
-                (self.column_toggling + value).rem_euclid(PLAYFIELD_COLS as isize)
-            }
-            _ => self.column_toggling,
-        };
-    }
-}
+    pub fn check_collision(&mut self) -> bool {
+        // TODO: надо переключать ход на следующую фигуру только когда колиззия происходит из-за
+        // гравитации, а не просто из-за прикосновения фигуры к чему-либо во время свободного
+        // падения
+        let mut moving_blocks = &self.current_tetramino.shape.blocks;
+        let mut stationary_blocks = &self.placed_blocks;
+        let arrangements = [[-1, 0], [1, 0], [0, 1], [0, -1]];
 
-#[derive(Clone)]
-pub struct Playfield {
-    pub field: Vec<Vec<Option<Block>>>,
+        for block in moving_blocks {
+            for arrangement in arrangements {
+                let neighbour_coords = block.coordinates + arrangement;
+                if stationary_blocks.contains(&Block {
+                    color: RED,
+                    coordinates: neighbour_coords,
+                }) || neighbour_coords.col >= 0
+                    && neighbour_coords.row >= 0
+                    && neighbour_coords.col < self.playfield_size.cols
+                    && neighbour_coords.row < self.playfield_size.rows
+                {
+                    return true;
+                }
+            }
+        }
+        false
+    }
+
+    pub fn place_current_tetramino(&mut self) -> bool {
+        self.placed_blocks
+            .extend(self.current_tetramino.shape.blocks.iter());
+        todo!("check if game over")
+    }
+
+    pub fn push_cur_tetramino(&mut self) {
+        self.current_tetramino.offset += OffsetPosition {
+            x_offset: 1,
+            y_offset: 1,
+        };
+        self.current_tetramino.offset %=
+            self.playfield_size - self.current_tetramino.shape.get_bounding_box();
+    }
 }
 
 #[derive(Clone, Copy)]
-pub struct Timer_ms {
+pub struct TimerMs {
     time_start: std::time::Instant,
     elapsed: i64,
     wait: i64,
 }
 
-impl Timer_ms {
+impl TimerMs {
     pub fn new(wait: i64) -> Self {
         Self {
             time_start: std::time::Instant::now(),
@@ -114,20 +313,11 @@ impl Timer_ms {
     }
 }
 
-impl Playfield {
-    fn new(rows: Option<usize>, cols: Option<usize>) -> Playfield {
-        Playfield {
-            field: vec![vec![None; rows.unwrap_or(PLAYFIELD_COLS)]; cols.unwrap_or(PLAYFIELD_ROWS)],
-        }
-    }
-}
-
 pub fn process_logic(game_state: &mut GameState, input: InputEvent) {
-    // let mut descend_delay = Timer_ms::new(10);
-    // descend_delay.update(frame_time_s.elapsed().as_millis() as i64);
-    // if descend_delay.is_out() {
-    // descend_delay.reset();
-    // frame_time_s = std::time::Instant::now();
-    game_state.toggle_block_in_row();
-    // }
+    if game_state.check_collision() {
+        game_state.place_current_tetramino();
+    } else {
+        game_state.push_cur_tetramino();
+    }
+    std::thread::sleep(Duration::from_millis(100));
 }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,91 @@
-use std::{
-    collections::HashSet,
-    sync::{mpsc, Arc, RwLock},
-    thread,
-    time::Duration,
-};
+use std::{cell, collections::HashSet};
 
-use macroquad::prelude::*;
-use tetrs::InputEvent;
-use tetrs::{process_logic, GameState};
+use macroquad::{color::Color, prelude::*};
+use tetrs::{process_logic, FallingTetramino, GameState, InputEvent};
+
+fn draw_current_tetramino(cur_tetramino: &FallingTetramino, grid: &SquareBitGridDrawer) {
+    for block in &cur_tetramino.shape.blocks {
+        grid.draw_grid_cell(block.coordinates.row + cur_tetramino.offset.y_offset, block.coordinates.col + cur_tetramino.offset.x_offset, block.color);
+    }
+}
+
+struct Coordinate {
+    x: f32,
+    y: f32,
+}
+
+struct GridSize {
+    rows: isize,
+    cols: isize,
+}
+
+struct SquareBitGridDrawer {
+    grid_size: GridSize,
+    deactivated_color: Color,
+    origin: Coordinate,
+    cell_size: f32,
+    grid_spacing: f32,
+}
+
+impl SquareBitGridDrawer {
+    fn new(
+        size: GridSize,
+        default_color: Color,
+        position_origin: Coordinate,
+        cell_size: f32,
+        cells_spacing: f32,
+    ) -> Self {
+        SquareBitGridDrawer {
+            grid_size: size,
+            deactivated_color: default_color,
+            origin: position_origin,
+            cell_size: cell_size,
+            grid_spacing: cells_spacing,
+        }
+    }
+
+    pub fn cell_origin(&self, row: isize, col: isize) -> Coordinate {
+        Coordinate {
+            x: col as f32 * self.cell_size + col as f32 * self.grid_spacing + self.origin.x,
+            y: row as f32 * self.cell_size + row as f32 * self.grid_spacing + self.origin.y,
+        }
+    }
+
+    pub fn draw_empty_grid(&self) {
+        for r in 0..self.grid_size.rows {
+            for c in 0..self.grid_size.cols {
+                draw_rectangle(
+                    self.cell_origin(r, c).x,
+                    self.cell_origin(r, c).y,
+                    self.cell_size,
+                    self.cell_size,
+                    self.deactivated_color,
+                );
+            }
+        }
+    }
+
+    pub fn draw_grid_cell(&self, row: isize, col: isize, color: Color) {
+        let cell_origin = Coordinate {
+            x: col as f32 * self.cell_size + col as f32 * self.grid_spacing + self.origin.x,
+            y: row as f32 * self.cell_size + row as f32 * self.grid_spacing + self.origin.y,
+        };
+        draw_rectangle(
+            cell_origin.x,
+            cell_origin.y,
+            self.cell_size,
+            self.cell_size,
+            color,
+        );
+    }
+}
+
+
+fn draw_game_frame(game_state: &GameState) {
+    let game_grid_drawer = SquareBitGridDrawer::new(GridSize { rows: game_state.playfield_size.rows, cols: game_state.playfield_size.cols },GRAY, Coordinate { x: 50., y: 50. }, 10.0, 5.0);
+    game_grid_drawer.draw_empty_grid();
+    draw_current_tetramino(&game_state.current_tetramino, &game_grid_drawer);
+}
 
 #[macroquad::main("MyGame")]
 async fn main() {
@@ -26,25 +104,7 @@ async fn main() {
         println!("Entered UI loop!");
         clear_background(BLACK);
 
-        let mut x_shift = 0.;
-        let mut y_shift = 50.;
-
-        for row in &game_state.playfield.field {
-            for col in row {
-                match col {
-                    Some(block) => {
-                        draw_rectangle(x_shift, y_shift, 10.0, 10.0, block.color);
-                    }
-                    None => {
-                        draw_rectangle(x_shift, y_shift, 10.0, 10.0, GRAY);
-                    }
-                }
-
-                x_shift += 15.;
-            }
-            y_shift += 15.;
-            x_shift = 0.;
-        }
+        draw_game_frame(&game_state);
 
         draw_fps();
         frame_counter += 1;
